d4e38dd6d425a35d370a1744cf468026d2859a01
debas-31 <debas31@gmail.com>
2022-08-02 17:20:52 +0300

    Add the remaing association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   has_many :groups
+  has_many :group_expenses, dependent: :destroy
+  has_many :expenses, through: :group_expenses
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
