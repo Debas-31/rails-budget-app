cc4b9985614d95e59b3c213854116195dd217b45
debas-31 <debas31@gmail.com>
2022-08-02 17:32:44 +0300

    Fix linter errors

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,7 +2,7 @@ class Group < ApplicationRecord
   belongs_to :user
   has_many :group_expenses, dependent: :destroy
   has_many :expenses, through: :group_expenses
-  
+
   validates :name, presence: true, length: { in: 1..20 }
   validates :icon, presence: true, length: { in: 1..100 }
 end
