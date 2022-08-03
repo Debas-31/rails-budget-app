d29150760e93dbc03fce4747d16bdf21362b07a3
debas-31 <debas31@gmail.com>
2022-08-03 11:44:40 +0300

    Change route to home_page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  get 'home_page/index'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "users#index"
+  root "home_page#index"
 end
