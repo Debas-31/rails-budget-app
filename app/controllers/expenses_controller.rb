bd370536f90ea8b165ada3911ddd337e76775155
debas-31 <debas31@gmail.com>
2022-08-03 23:16:13 +0300

    Add a method to create a transaction

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -5,4 +5,22 @@ class ExpensesController < ApplicationController
     @group.group_expenses.each do |group_expense|
       @expenses.push(group_expense.expense)
   end
+
+  def new
+    @expense = Expense.new
+  end
+
+  def Create
+    @new_expense = current_user.expense.new(expense_params)
+    if @new_expense.save
+      GroupExpense.create(group_id: params[:group_id], expense_id: @new_expense.id)
+      redirect_to group_expenses_path(params[:group_id]), flash: { alert: 'Your transaction is registered' }
+    else
+      redirect_to new_group-expense_path, flash: { alert: 'Could not register your transaction'}
+    end
+  end
+
+  def expense_params
+    params.require(:group).permit(:name, :amount)
+  end
 end
