6606c97433655f7b68bc7258d61cd90018fdfdfc
debas-31 <debas31@gmail.com>
2022-08-03 18:51:37 +0300

    Add index for expense views

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,4 +1,8 @@
 class ExpensesController < ApplicationController
   def index
+    @group = Group.find(params[:group_id])
+    @expenses = []
+    @group.group_expenses.each do |group_expense|
+      @expenses.push(group_expense.expense)
   end
 end
