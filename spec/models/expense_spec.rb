e844929c4af96fa6cfde488a76713a2a65fc288c
debas-31 <debas31@gmail.com>
2022-08-04 18:42:54 +0300

    Add test for expense model

--- spec/models/expense_spec.rb
+++ spec/models/expense_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe Expense, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Expense model' do
+    user = User.create(name: 'Debas', lastname: 'Gebrengus', email: 'debas@example.com', password: '123456')
+    subject { Expense.new(user_id: user, name: 'food', amount: 120) }
+    before { subject.save }
+
+    it 'check the name is not blank' do
+      subject.name = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'check if the name is not exceeding 20 characters' do
+      subject.name = 'Hello world Hello world Hello world'
+      expect(subject).to_not be_valid
+    end
+
+    it 'check the amount is not blank' do
+      subject.amount = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'check if amount is numeric' do
+      subject.amount = 'not-numeric'
+      expect(subject).to_not be_valid
+    end
+
+    it 'check if price is equal or greater than one' do
+      expect(subject.amount).to be >= 1
+    end
+  end
 end
