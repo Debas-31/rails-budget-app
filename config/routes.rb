02210c6da7f7d55d2c7f4dd6fc35b15754340836
debas-31 <debas31@gmail.com>
2022-08-04 11:33:29 +0300

    Add routes to expenses, and as authenticated to the group#index

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  resources :groups, only: [:index, :new, :create] do
-    resources :expenses, only: [:index]
+  authenticate :user do
+    root to: "group#index", as: :authenticated_root
   end
   
+  resources :groups, only: [:index, :new, :create] do
+    resources :expenses, only: [:index, :new, :create]
+  end
+
   # Defines the root path route ("/")
   root "home_page#index"
 end
