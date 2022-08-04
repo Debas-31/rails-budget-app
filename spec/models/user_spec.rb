6fdeea3f53f04ee7ef6ca2de117d21041da2f9a6
debas-31 <debas31@gmail.com>
2022-08-04 18:37:52 +0300

    Add test for group model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'User model' do
-    subject { User.new(name: 'Debas', email: 'debas@example.com', password: '123456') }
+    subject { User.new(name: 'Debas', lastname: 'Gebrengus' email: 'debas@example.com', password: '123456') }
     before { subject.save }
 
     it 'check the name is not blank' do
@@ -10,6 +10,11 @@ RSpec.describe User, type: :model do
       expect(subject).to_not be_valid
     end
 
+    it 'check the surname is not blank' do
+      subject.surname = nil
+      expect(subject).to_not be_valid
+    end
+    
     it 'check the email is not blank' do
       subject.email = nil
       expect(subject).to_not be_valid
